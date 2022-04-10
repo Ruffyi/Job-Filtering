f33e71edf81977e81aae4b532d90503d5113c86c
Kamil <kam.nas21@wp.pl>
2022-04-10 11:12:16 +0200

    Add dummy data json file, add custom fetch hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import useFetch from './hooks/useFetch';
+
 import Header from './layouts/Header/Header';
 import Root from './layouts/Root/Root';
 
 const App = () => {
+	const { data } = useFetch('data/data.json');
+
+	console.log(data);
+
 	return (
 		<Root>
 			<Header />
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = <T>(url: string) => {
+	const [data, setData] = useState<T | []>([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState('');
+
+	const fetchData = async () => {
+		setLoading(true);
+		try {
+			const response = await axios(url, {
+				method: 'GET',
+			});
+			const data = response.data;
+			setData(data);
+			setLoading(false);
+		} catch (error) {
+			if (error instanceof Error) {
+				setError(error.message);
+			}
+		}
+		// setData(data);
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, [url]);
+
+	return { data, loading, error } as const;
+};
+
+export default useFetch;
